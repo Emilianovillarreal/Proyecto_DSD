433bf9300468fd4c424eb632f28c6ea01d858179
Emiliano Villarreal <emiliano02villarreal@gmail.com>
2022-11-14 00:31:59 -0300

    Bloque Principal: FUNCIONAL

--- frecuencimetro.vhd
+++ frecuencimetro.vhd
@@ -13,9 +13,9 @@ end frecuencimetro;
 
 architecture Behavioral of frecuencimetro is
 
-signal s_count_reach, aux : std_logic;
+signal s_count_reach,s_count_reach2, aux : std_logic;
 
-signal s_count,count_freeze: std_logic_vector(13 downto 0);
+signal s_count,s_count_freeze: std_logic_vector(13 downto 0);
 signal s_frequency : std_logic_vector(14 downto 0);
 
 signal s_selection : std_logic_vector(3 downto 0);
@@ -29,7 +29,17 @@ port map
 	RST => RST,
 	o_count_reach => s_count_reach
 );
-aux <= s_count_reach or RST;
+
+FLIP_FLOP1: process(clk, s_count_reach)
+begin
+	if rising_edge(clk) then 
+		s_count_reach2 <= s_count_reach;
+	end if;
+end process;
+
+
+aux <= s_count_reach2 or RST;
+
 
 Count_square_sign_pulses: entity work.CONTADOR_2
 port map
@@ -40,8 +50,8 @@ port map
 
 flip_flop:process(clk,s_count_reach)
 begin
-	if rising_edge(clk) and s_count_reach = '1' then 
-		count_freeze <= s_count;
+	if (rising_edge(clk) and (s_count_reach = '1'))then 
+		s_count_freeze <= s_count;
 	end if;
 end process;
 
@@ -49,7 +59,8 @@ frequency_calculation: entity work.DESPLAZAMIENTO_IZQ_V3
 port map
 (	CLK => CLK,
 	RST => RST,
-	count_frezze => s_count,
+	count_frezze =>s_count_freeze,
+	--count_frezze => s_count,
 	Frequency => s_frequency
 );
 
@@ -62,9 +73,9 @@ port map
 DECO: entity work.deco
 port map
 (	Entrada => s_selection,
+	
 	Salida => SALIDA,
 	anodos => anodo
 );
 
 end Behavioral;
-
